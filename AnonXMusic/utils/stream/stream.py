8a0228bfc4aa87c227de0df51f5966f3d0ff333a
damian-bots <tosu0791@gmail.com>
2026-03-13 00:55:30 +0530

    Update stream.py

--- AnonXMusic/utils/stream/stream.py
+++ AnonXMusic/utils/stream/stream.py
@@ -190,7 +190,7 @@ async def stream(
                 original_chat_id,
                 photo=img,
                 caption=_["stream_1"].format(
-                    f"https://t.me/{app.username}?start=info_{vidid}",
+                    f"https://youtube.com/watch?v={vidid}",
                     title[:23],
                     duration_min,
                     user_name,
@@ -358,7 +358,7 @@ async def stream(
                 original_chat_id,
                 photo=img,
                 caption=_["stream_1"].format(
-                    f"https://t.me/{app.username}?start=info_{vidid}",
+                    f"https://youtube.com/watch?v={vidid}",
                     title[:23],
                     duration_min,
                     user_name,
